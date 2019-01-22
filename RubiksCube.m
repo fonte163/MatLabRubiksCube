909e3937fa0c3f03feeb9672820d5f13497ce50a
NexAdn <nexadn@yandex.com>
2019-01-22 22:59:51 +0100

    Continue refactoring
    I hope this works :/

--- RubiksCube.m
+++ RubiksCube.m
@@ -1,5 +1,10 @@
 function RubiksCube()
 
+global fig;
+global elon;
+global azim;
+global cmenu;
+
 % Farbreihenfolge: w,r,b,o,gr,ge
 % Würfel halten, dass dich die rote Fläche anguckt, weiß oben, weiß und rot wie man sie sieht, für b,o,gr würfel drehen und für ge Würfel von Ausgangsposition nach oben kippen
 % dann Flächenreihenfolge: ol, om, or, rm, ur, um, ul, lm
@@ -36,17 +41,22 @@ for i = 1:6
 	end
 end
 
-% also z.B. sollte pos(1,1) die Eckpunke der oberen linken weißen Fläche geben, genauer gesagt [0,1,1,0] als x-Koordinaten, [3,3,2,2] als y und [3,3,3,3] als z
-figure('Name','Rubiks Cube','NumberTitle','off','MenuBar','none');
-elon = get_elon_ui();
-azim = get_azim_ui();
+function main()
+	% also z.B. sollte pos(1,1) die Eckpunke der oberen linken weißen Fläche geben, genauer gesagt [0,1,1,0] als x-Koordinaten, [3,3,2,2] als y und [3,3,3,3] als z
+	fig = figure('Name','Rubiks Cube','NumberTitle','off','MenuBar','none');
+	elon = get_elon_ui();
+	azim = get_azim_ui();
 
-ui_setup();
+	ui_setup();
+	cmenu = generate_and_patch_ui_menu();
+
+	main_loop();
+end
 
 function ui_setup()
 	uicontrol('Style', 'text', 'Position', [130 20 60 20], 'String', 'elongation');
 	uicontrol('Style', 'text', 'Position', [130 40 50 20], 'String', 'azimuth');
-	rotateview;
+	rotate_view();
 	axis off
 	axis equal
 
@@ -62,7 +72,9 @@ function a=get_azim_ui()
 end
 
 function rotate_view(~, ~)
-   view([(elon.Value*360) (azim.Value*180-90)]);
+	global elon;
+	global azim;
+	view( get(azim, 'Value')*180-90,get(elon, 'Value')*360);
 end
 
 function generate_centerpieces()
@@ -74,27 +86,46 @@ function generate_centerpieces()
 	patch([1,2,2,1],[1,1,2,2],[0,0,0,0],'y');
 end
 % restliche Flächen
-function ui_label_and_patch_items()
+function cmenu=generate_and_patch_ui_menu()
     for i = 1:6
         for j = 1:8
-            cmenu(i, j) = ui_label_item(i, j);
-            ui_label_generate_patch(cmenu, i, j);
+            cmenu(i, j) = generate_single_ui_menu(i, j);
         end
     end
+	update_patches(cmenu);
 end
 
-function cmenu=ui_label_item(i, j)
+function cmenu=generate_single_ui_menu(i, j)
 	cmenu(i,j) = uicontextmenu;
-	uimenu(cmenu(i,j), 'Label', 'white', 'Callback', @changeColor, 'UserData', [i j]);
-	uimenu(cmenu(i,j), 'Label', 'red', 'Callback', @changeColor, 'UserData', [i j]);
-	uimenu(cmenu(i,j), 'Label', 'blue', 'Callback', @changeColor, 'UserData', [i j]);
-	uimenu(cmenu(i,j), 'Label', 'orange', 'Callback', @changeColor, 'UserData', [i j]);
-	uimenu(cmenu(i,j), 'Label', 'green', 'Callback', @changeColor, 'UserData', [i j]);
-	uimenu(cmenu(i,j), 'Label', 'yellow', 'Callback', @changeColor, 'UserData', [i j]);
+	uimenu(cmenu(i,j), 'Label', 'white', 'Callback', @change_color, 'UserData', [i j]);
+	uimenu(cmenu(i,j), 'Label', 'red', 'Callback', @change_color, 'UserData', [i j]);
+	uimenu(cmenu(i,j), 'Label', 'blue', 'Callback', @change_color, 'UserData', [i j]);
+	uimenu(cmenu(i,j), 'Label', 'orange', 'Callback', @change_color, 'UserData', [i j]);
+	uimenu(cmenu(i,j), 'Label', 'green', 'Callback', @change_color, 'UserData', [i j]);
+	uimenu(cmenu(i,j), 'Label', 'yellow', 'Callback', @change_color, 'UserData', [i j]);
 end
 
-function ui_label_generate_patch(cmenu, i, j)
-	patch(pos(i,(12*j-11):(12*j-8)), pos(i,(12*j-7):(12*j-4)), pos(i,(12*j-3):(12*j)), w(i,j,:), 'UIContextMenu', cmenu(i,j), 'UserData', [i j]);
+function change_color(source, ~)
+	global cmenu;
+    i = source.UserData(1);
+    j = source.UserData(2);
+	switch source.Label
+        case 'white'
+            w(i,j,:) = c(1,:);
+        case 'red'
+            w(i,j,:) = c(2,:);
+        case 'blue'
+            w(i,j,:) = c(3,:);
+        case 'orange'
+            w(i,j,:) = c(4,:);
+        case 'green'
+            w(i,j,:) = c(5,:);
+        case 'yellow'
+            w(i,j,:) = c(6,:);
+    end
+    % patch(pos(i,(12*j-11):(12*j-8)), pos(i,(12*j-7):(12*j-4)), pos(i,(12*j-3):(12*j)), w(i,j,:), 'UIContextMenu', cmenu(i,j), 'UserData', [i j]);
+	update_single_patch(cmenu, i, j);
+    refresh;
 end
 
 pause(10);
@@ -115,102 +146,113 @@ function single_frame_wait()
 	pause(1);
 end
 
-	function changeColor(source, ~)
-        i = source.UserData(1);
-        j = source.UserData(2);
-		switch source.Label
-            case 'white'
-                w(i,j,:) = c(1,:);
-            case 'red'
-                w(i,j,:) = c(2,:);
-            case 'blue'
-                w(i,j,:) = c(3,:);
-            case 'orange'
-                w(i,j,:) = c(4,:);
-            case 'green'
-                w(i,j,:) = c(5,:);
-            case 'yellow'
-                w(i,j,:) = c(6,:);
-        end
-        patch(pos(i,(12*j-11):(12*j-8)), pos(i,(12*j-7):(12*j-4)), pos(i,(12*j-3):(12*j)), w(i,j,:), 'UIContextMenu', cmenu(i,j), 'UserData', [i j]);
-        refresh;
+function turn(dir)
+	global cmenu;
+    % R, R', U, U', L, L', D, D', F, F', B, B'
+    for i = 0:1
+        arrs(i+1) = patch(arrows(dir, (1+9*i):(3+9*i)), arrows(dir, (4+9*i):(6+9*i)), arrows(dir, (7+9*i):(9+9*i)), [0.5 0.1 0.5]);
     end
+    pause(1);
+    switch dir
+    case 1
+        turn_r();
+    case 2
+        reverse_turn_r();
+    case 3
+        turn_u();
+    case 4
+        reverse_turn_u();
+    end
+    update_patches(cmenu);
+    hide_arrows(arrs);
+    refresh;
+end
 
-    function turn(dir)
-        % R, R', U, U', L, L', D, D', F, F', B, B'
-        for i = 0:1
-            arrs(i+1) = patch(arrows(dir, (1+9*i):(3+9*i)), arrows(dir, (4+9*i):(6+9*i)), arrows(dir, (7+9*i):(9+9*i)), [0.5 0.1 0.5]);
+function turn_r()
+	buf1 = w(1, 3, :);
+	buf2 = w(1, 4, :);
+	buf3 = w(1, 5, :);
+	w(1, 3:5, :) = w(2, 3:5, :);
+	w(2, 3:5, :) = w(6, 3:5, :);
+	w(6, 3:4, :) = w(4, 7:8, :);
+	w(6, 5, :) = w(4, 1, :);
+	w(4, 7, :) = buf1;
+	w(4, 8, :) = buf2;
+	w(4, 1, :) = buf3;
+	for j = 1:2
+		buf = w(3,8,:);
+		for i = 8:-1:2
+			w(3,i,:) = w(3,i-1,:);
+		end
+		w(3,1,:) = buf;
+	end
+end
+
+function reverse_turn_r()
+	buf = w(1, 3:5, :);
+	w(1, 3:4, :) = w(4, 7:8, :);
+	w(1, 5, :) = w(4, 1, :);
+	w(4, 7:8, :) = w(6, 3:4, :);
+	w(4, 1, :) = w(6, 5, :);
+	w(6, 3:5, :) = w(2, 3:5, :);
+	w(2, 3:5, :) = buf;
+	for j = 1:2
+		buf = w(3,1,:);
+		for i = 1:7
+			w(3,i,:) = w(3,i+1,:);
+		end
+		w(3,8,:) = buf;
+	end
+end
+
+function turn_u()
+	buf = w(2, 1:3, :);
+	for i = 2:4
+	w(i, 1:3, :) = w(i+1, 1:3, :);
+	end
+	w(5, 1:3, :) = buf;
+	for i = 1:2
+		buf = w(1,8,:);
+		for i = 8:-1:2
+			w(1,i,:) = w(1,i-1,:);
+		end
+		w(1,1,:) = buf;
+	end
+end
+
+function reverse_turn_u()
+	buf = w(5, 1:3, :);
+	for i = 5:-1:3
+	w(i, 1:3, :) = w(i-1, 1:3, :);
+	end
+	w(2, 1:3, :) = buf;
+	for j = 1:2
+		buf = w(1,1,:);
+		for i = 1:7
+			w(1,i,:) = w(1,i+1,:);
+		end
+		w(1,8,:) = buf;
+	end
+end
+
+function update_patches(cmenu)
+	for i = 1:6
+        for j = 1:8
+            update_single_patch(cmenu, i, j);
         end
-        pause(1);
-        switch dir
-            case 1
-                buf1 = w(1, 3, :);
-                buf2 = w(1, 4, :);
-                buf3 = w(1, 5, :);
-                w(1, 3:5, :) = w(2, 3:5, :);
-                w(2, 3:5, :) = w(6, 3:5, :);
-                w(6, 3:4, :) = w(4, 7:8, :);
-                w(6, 5, :) = w(4, 1, :);
-                w(4, 7, :) = buf1;
-                w(4, 8, :) = buf2;
-                w(4, 1, :) = buf3;
-                for j = 1:2
-                buf = w(3,8,:);
-                for i = 8:-1:2
-                    w(3,i,:) = w(3,i-1,:);
-                end
-                w(3,1,:) = buf;
-                end
-            case 2
-                buf = w(1, 3:5, :);
-                w(1, 3:4, :) = w(4, 7:8, :);
-                w(1, 5, :) = w(4, 1, :);
-                w(4, 7:8, :) = w(6, 3:4, :);
-                w(4, 1, :) = w(6, 5, :);
-                w(6, 3:5, :) = w(2, 3:5, :);
-                w(2, 3:5, :) = buf;
-                for j = 1:2
-                    buf = w(3,1,:);
-                    for i = 1:7
-                        w(3,i,:) = w(3,i+1,:);
-                    end
-                    w(3,8,:) = buf;
-                end
-            case 3
-                buf = w(2, 1:3, :);
-                for i = 2:4
-                w(i, 1:3, :) = w(i+1, 1:3, :);
-                end
-                w(5, 1:3, :) = buf;
-                for i = 1:2
-                    buf = w(1,8,:);
-                    for i = 8:-1:2
-                        w(1,i,:) = w(1,i-1,:);
-                    end
-                    w(1,1,:) = buf;
-                end
-            case 4
-                buf = w(5, 1:3, :);
-                for i = 5:-1:3
-                w(i, 1:3, :) = w(i-1, 1:3, :);
-                end
-                w(2, 1:3, :) = buf;
-                for j = 1:2
-                    buf = w(1,1,:);
-                    for i = 1:7
-                        w(1,i,:) = w(1,i+1,:);
-                    end
-                    w(1,8,:) = buf;
-                end
-        end
-        for i = 1:6
-            for j = 1:8
-                patch(pos(i,(12*j-11):(12*j-8)), pos(i,(12*j-7):(12*j-4)), pos(i,(12*j-3):(12*j)), w(i,j,:), 'UIContextMenu', cmenu(i,j), 'UserData', [i j]);
-            end
-        end
-        for i = 0:1
-            arrs(i+1).Visible = 'Off';
-        end
-        refresh;
     end
+end
+
+function update_single_patch(cmenu, i, j)
+	patch(pos(i,(12*j-11):(12*j-8)), pos(i,(12*j-7):(12*j-4)), pos(i,(12*j-3):(12*j)), w(i,j,:), 'UIContextMenu', cmenu(i,j), 'UserData', [i j]);
+end
+
+function hide_arrows(arrows)
+	for i = 0:1
+        arrows(i+1).Visible = 'Off';
+    end
+end
+
+main()
+
 end
